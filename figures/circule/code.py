a1a5fe3af7efbaaaa649e45955cc71c071011bde
User <Lizunova.violetta@bk.ru>
2023-02-07 16:48:59 +0300

    Initial commit

---
+++ figures/circule/code.py
@@ -0,0 +1,23 @@
+from math import pi
+
+def_radius = 5
+
+def circle_perimetr(radius=def_radius):
+    return 2 * pi * radius
+
+def circle_area(radius=def_radius):
+    return pi * radius ** 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
